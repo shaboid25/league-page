3d7d63be1f438f15c7b06198e2078b1bc7e0c391
shaboid25 <jconley@jacobconley.com>
2025-01-24 13:36:22 -0800

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "thebigthangtheory"; // your league ID
-export const leagueName = "The Big Thangang Theory"; // your league name
+export const leagueID = "The Bid Thang Theory"; // your league ID
+export const leagueName = "The Big Thang Theory"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
