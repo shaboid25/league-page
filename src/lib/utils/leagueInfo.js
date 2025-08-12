eadffb5d9bddfc32f32d56c210e9eb3bababe225
shaboid25 <jconley@jacobconley.com>
2025-08-12 09:35:47 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -34,11 +34,11 @@ export const managers = [
     "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
     "name": "Jake Conley",
     "managerID" : "shaboid",
-    "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+    "tookOver": "null", // (optional) used if a manager took over a team, delete this line or change to null otherwise
     "location": "Stayton, OR", // (optional)
     "bio": "Lorem ipsum...",
     "photo": "/managers/Dirty-Birds.jpg", // square ratio recommended (no larger than 500x500)
-    "fantasyStart": null, // (optional) when did the manager start playing fantasy football
+    "fantasyStart": "null", // (optional) when did the manager start playing fantasy football
      "favoriteTeam": "lac", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
     "rival": {
